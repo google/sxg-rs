bbf33d44f7d787ef4606fc6daecd551633278558
Boxiao Cao <9083193+antiphoton@users.noreply.github.com>
2021-04-26 23:36:17 -0400

    Fix bugs in parsing accept header (#22)
    * Fix a bug of triming whitespace in media-type
    
    * Fix a bug that ignored upper case header names
    
    * Allow sxg version to be quoted string
    
    * Ingnore v=b3 that is not in accept extension params

--- sxg_rs/src/media_type.rs
+++ sxg_rs/src/media_type.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Eq, PartialEq)]
 struct MediaType {
     weight_milliles: u16,
     is_sxg_b3: bool,
@@ -6,26 +7,32 @@ struct MediaType {
 impl<'a> MediaType {
     fn new(input: &'a str) -> Self {
         let input = input.trim();
-        let tokens: Vec<_> = input.split(';').collect();
+        let tokens: Vec<_> = input.split(';').map(|x| x.trim()).collect();
         let is_sxg = tokens[0].eq_ignore_ascii_case("application/signed-exchange");
         let mut weight_milliles = 1000;
         let mut version = None;
         for param in tokens.iter().skip(1) {
-            let entry: Vec<_> = param.trim().split('=').collect();
+            let entry: Vec<_> = param.split('=').collect();
             if entry.len() == 2 {
                 let name = entry[0].trim();
                 let value = entry[1].trim();
-                if name == "q" {
+                if name.eq_ignore_ascii_case("q") {
                     if let Ok(value) = value.parse::<f64>() {
                         weight_milliles = (value * 1000.0) as u16;
+                        // https://tools.ietf.org/html/rfc7231#section-5.3.2
+                        // The weight "q=..." separates media type parameters
+                        // from accept extension paramters.
+                        // The "v=b3" that occurs after "q=..." is not part
+                        // of the media type.
+                        break;
                     }
-                } else if name == "v" {
+                } else if name.eq_ignore_ascii_case("v") {
                     version = Some(value);
                 }
             }
         }
         MediaType {
-            is_sxg_b3: is_sxg && version == Some("b3"),
+            is_sxg_b3: is_sxg && (version == Some("b3") || version == Some(r#""b3""#)),
             weight_milliles,
         }
     }
@@ -55,6 +62,64 @@ mod tests {
         assert!(validate_sxg_request_header("application/signed-exchange;v=b3;q=0.9,*/*;q=0.8").is_ok());
         assert!(validate_sxg_request_header("").is_err());
         assert!(validate_sxg_request_header("text/html,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9").is_err());
+    }
+    #[test]
+    fn opional_whitespaces() {
+        assert!(validate_sxg_request_header("  application/signed-exchange  ;  v=b3  ;  q=0.9  ,  */*  ;  q=0.8  ").is_ok());
+    }
+    #[test]
+    fn upper_case_param_name() {
+        assert_eq!(
+            MediaType::new("application/signed-exchange;V=b3;Q=0.9"),
+            MediaType {
+                is_sxg_b3: true,
+                weight_milliles: 900,
+            },
+        );
+    }
+    #[test]
+    fn v_not_b3() {
+        assert_eq!(
+            MediaType::new("application/signed-exchange"),
+            MediaType {
+                is_sxg_b3: false,
+                weight_milliles: 1000,
+            },
+        );
+        assert_eq!(
+            MediaType::new("application/signed-exchange;v=b2"),
+            MediaType {
+                is_sxg_b3: false,
+                weight_milliles: 1000,
+            },
+        );
+    }
+    #[test]
+    fn quoted_string() {
+        assert_eq!(
+            MediaType::new(r#"application/signed-exchange;v="b3""#),
+            MediaType {
+                is_sxg_b3: true,
+                weight_milliles: 1000,
+            },
+        );
+    }
+    #[test]
+    fn does_not_count_v_after_q() {
+        assert_eq!(
+            MediaType::new("application/signed-exchange;v=b3;q=0.5"),
+            MediaType {
+                is_sxg_b3: true,
+                weight_milliles: 500,
+            },
+        );
+        assert_eq!(
+            MediaType::new("application/signed-exchange;q=0.5;v=b3"),
+            MediaType {
+                is_sxg_b3: false,
+                weight_milliles: 500,
+            },
+        );
     }
 }
 
