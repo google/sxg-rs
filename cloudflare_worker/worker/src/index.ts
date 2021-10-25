google/sxg-rs

commit bea1deecb83f51f0db40cf3c8e75065cff55b382

Add SXGOnly processor. (#99)
Add two syntaxes for SXG-only behavior, `<template data-sxg-only>` and `<script
data-issxg-var>`.

cloudflare_worker/worker/src/index.ts
@@ -250,6 +250,7 @@ async function handleRequest(request: Request) {
     }
     sxgPayload = await processHTML(sxgPayload, [
       new PromoteLinkTagsToHeaders,
+      new SXGOnly(true),
     ]);
     let response = await generateSxgResponse(fallbackUrl, certOrigin, sxgPayload);
     fallback.body?.cancel();
@@ -262,6 +263,9 @@ async function handleRequest(request: Request) {
         // simply use all http headers from the user.
         fallback = await fetch(request);
     }
+    fallback = await processHTML(fallback, [
+      new SXGOnly(false),
+    ]);
     if (worker.shouldRespondDebugInfo() && e.toString) {
       let message = e.toString();
       return new Response(
@@ -299,9 +303,8 @@ const HTML = /^text\/html([ \t]*;[ \t]*charset=(utf-8|"utf-8"))?$/i;
 interface HTMLProcessor {
   register(rewriter: HTMLRewriter): void;
   // Returns true iff the processor modified the HTML body. processHTML uses
-  // the rewritten HTML body iff one of the processors returns true. This
-  // function should not have any side-effects; it might not run.
-  modified(): boolean;
+  // the rewritten HTML body iff one of the processors returns true.
+  modified: boolean;
   onEnd(payload: Response): void;
 }
 
@@ -309,6 +312,7 @@ interface HTMLProcessor {
 // Later, generateSxgResponse will further modify the link header to support
 // SXG preloading of eligible subresources.
 class PromoteLinkTagsToHeaders implements HTMLProcessor {
+  modified: boolean = false;
   link_tags: {href: string, as: string}[] = [];
   register(rewriter: HTMLRewriter): void {
     rewriter.on('link[rel~="preload" i][href][as]', {
@@ -323,14 +327,46 @@ class PromoteLinkTagsToHeaders implements HTMLProcessor {
       },
     });
   }
-  modified(): boolean {
-    return false;
-  }
   onEnd(payload: Response): void {
     if (this.link_tags.length) {
       const link = this.link_tags.map(({href, as}) => `<${href}>;rel=preload;as=${as}`).join(',');
       payload.headers.append('Link', link);
     }
+  }
+}
+
+// Provides two syntaxes for SXG-only behavior:
+//
+// For `<template data-sxg-only>` elements:
+// - If SXG, they are "unwrapped" (i.e. their children promoted out of the <teplate>).
+// - Else, they are deleted.
+//
+// For `<meta name=declare-issxg-var>` elements, they are replaced with
+// `<script>window.isSXG=...</script>`, where `...` is true or false.
+class SXGOnly {
+  isSXG: boolean;
+  modified: boolean = false;
+  constructor(isSXG: boolean) {
+    this.isSXG = isSXG;
+  }
+  register(rewriter: HTMLRewriter): void {
+    rewriter.on('script[data-issxg-var]', {
+      element: (script: Element) => {
+        script.setInnerContent(`window.isSXG=${this.isSXG}`);
+        this.modified = true;
+      },
+    }).on('template[data-sxg-only]', {
+      element: (template: Element) => {
+        if (this.isSXG) {
+          template.removeAndKeepContent();
+        } else {
+          template.remove();
+        }
+        this.modified = true;
+      },
+    });
+  }
+  onEnd(_payload: Response): void {
   }
 }
 
@@ -419,7 +455,7 @@ async function processHTML(payload: Response, processors: HTMLProcessor[]): Prom
   // handler for XML declarations, so we skip the check.
 
   if (known_utf8) {
-    if (processors.some((p) => p.modified())) {
+    if (processors.some((p) => p.modified)) {
       payload = new Response(modifiedBody, {
         status: payload.status,
         statusText: payload.statusText,

commit 7d8b9202b3d41e30ae212e6d60b8aed34046c5af

Fix exception on HTML >8MB.
Fall back to unprocessed HTML if the processHTML call fails on the fallback
payload. Fixes a bug introduced by commit bea1deec.

cloudflare_worker/worker/src/index.ts
@@ -263,9 +263,18 @@ async function handleRequest(request: Request) {
         // simply use all http headers from the user.
         fallback = await fetch(request);
     }
-    fallback = await processHTML(fallback, [
-      new SXGOnly(false),
-    ]);
+    let fallwayback;
+    [fallback, fallwayback] = teeResponse(fallback);
+    try {
+      // If the body is HTML >8MB, processHTML will fail.
+      fallback = await processHTML(fallback, [
+        new SXGOnly(false),
+      ]);
+      fallwayback.body?.cancel();
+    } catch {
+      fallback.body?.cancel();
+      fallback = fallwayback;
+    }
     if (worker.shouldRespondDebugInfo() && e.toString) {
       let message = e.toString();
       return new Response(