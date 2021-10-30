4ef7b7965d300db7ea9beb22cbc13523ce612634
Devin Mullins <twifkak@google.com>
2021-10-29 17:52:24 -0700

    Fix streams test flake. (#112)
    `writer.close()` was consistently throwing an exception `TypeError: Cannot
    close a ERRORED writable stream`. The exception was only sometimes being
    noticed by Jasmine because it was not being `await`ed, and thus was falling
    back to Jasmine's global detector for unhandled promise rejections. This only
    succeeded if the test harness wasn't destructed before the `close()` call
    finished.
    
    Changed it not to call `close()` in this case, and to always call `await` to
    make future such test issues non-flaky.

--- cloudflare_worker/worker/src/streams.test.ts
+++ cloudflare_worker/worker/src/streams.test.ts
@@ -31,13 +31,16 @@ describe('readIntoArray', () => {
   });
   it('reads a stream with two chunks', async () => {
     const {writable, readable} = new TransformStream;
-    const writer = writable.getWriter();
-    for (let i = 0; i < 2; i++) {
-      writer.write(new TextEncoder().encode('hello'));
-    }
-    writer.close();
+    const write = (async() => {
+      const writer = writable.getWriter();
+      for (let i = 0; i < 2; i++) {
+        await writer.write(new TextEncoder().encode('hello'));
+      }
+      await writer.close();
+    })();
     const output = await readIntoArray(readable, 0x1000);
     expect(output).toEqual(new TextEncoder().encode('hellohello'));
+    await write;
   });
   it('errors if stream > maxSize', async () => {
     const input = new Response('hello').body;
@@ -46,12 +49,15 @@ describe('readIntoArray', () => {
   });
   it('errors if second chunk > maxSize', async () => {
     const {writable, readable} = new TransformStream;
-    const writer = writable.getWriter();
-    for (let i = 0; i < 2; i++) {
-      writer.write(new TextEncoder().encode('hello'));
-    }
-    writer.close();
+    const write = (async () => {
+      const writer = writable.getWriter();
+      for (let i = 0; i < 2; i++) {
+        await writer.write(new TextEncoder().encode('hello'));
+      }
+      // Don't close the writer; it'll be cancelled by readIntoArray.
+    })();
     const output = await readIntoArray(readable, 7);
     expect(output).toBe(null);
+    await write;
   });
 })
