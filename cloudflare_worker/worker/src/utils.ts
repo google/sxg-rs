6167eb8d84fe696cbaf2d9ac994710c7e9336e89
Devin Mullins <twifkak@google.com>
2022-02-23 11:23:06 -0800

    Include additional allowed params in Link headers. (#151)
    When promoting link tags to headers, include all the parameters allowed by
    https://github.com/google/webpackager/blob/main/docs/cache_requirements.md.
    
    This should allow fonts and XHRs to be prefetched, as they require
    crossorigin=anonymous.

--- cloudflare_worker/worker/src/utils.ts
+++ cloudflare_worker/worker/src/utils.ts
@@ -14,8 +14,30 @@
  * limitations under the License.
  */
 
+// https://datatracker.ietf.org/doc/html/rfc7230#section-3.2.6
+export const TOKEN = /^[!#$%&'*+.^_`|~0-9a-zA-Z-]+$/;
+
 export function arrayBufferToBase64(buffer: ArrayBuffer): string {
   const data = Array.from(new Uint8Array(buffer));
   const s = data.map(x => String.fromCharCode(x)).join('');
   return btoa(s);
+}
+
+// Strings representable in a quoted-string, per
+// https://datatracker.ietf.org/doc/html/rfc7230#section-3.2.6 (including \x22
+// ["] and \x5C [\])..
+const ALLOWED_QUOTED_STRING_VALUE = /^[\t \x21-\x7E]*$/;
+
+// Escapes a value for use as a Link header param, per
+// https://datatracker.ietf.org/doc/html/rfc8288#section-3.
+export function escapeLinkParamValue(value: string): string | null {
+  if (value.match(TOKEN)) {
+    return value;
+  } else if (value.match(ALLOWED_QUOTED_STRING_VALUE)) {
+    return '"' + [...value].map((char) =>
+      (char === '\\' || char === '"') ? '\\' + char : char
+    ).join('') + '"';
+  } else {
+    return null;
+  }
 }
