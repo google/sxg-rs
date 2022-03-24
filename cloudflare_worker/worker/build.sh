307295cfb58d17dc26d750140ace4abff67ce46f
Boxiao Cao <9083193+antiphoton@users.noreply.github.com>
2022-03-24 12:14:51 -0700

    Use wildcard in build.sh

--- cloudflare_worker/worker/build.sh
+++ cloudflare_worker/worker/build.sh
@@ -1,3 +1,2 @@
 node_modules/.bin/esbuild src/index.ts --bundle --platform=browser --outfile=dist/index.js
-node_modules/.bin/esbuild src/streams.test.ts --bundle --platform=browser --outfile=dist/streams.test.js
-node_modules/.bin/esbuild src/utils.test.ts --bundle --platform=browser --outfile=dist/utils.test.js
+node_modules/.bin/esbuild src/*.test.ts --bundle --platform=browser --outdir=dist
