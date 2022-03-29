dce785931bdeea4207dbee102f09cbb2b02b38ed
Boxiao Cao <9083193+antiphoton@users.noreply.github.com>
2022-03-29 13:41:23 -0700

    Parse content type header

--- sxg_rs/src/process_html.rs
+++ sxg_rs/src/process_html.rs
@@ -16,6 +16,8 @@ use crate::http::HttpResponse;
 use crate::http_parser::link::Link;
 use crate::link::ALLOWED_PARAM_NAMES;
 use anyhow::Result;
+use once_cell::sync::Lazy;
+use regex::Regex;
 use serde::Deserialize;
 use std::borrow::Cow;
 
@@ -25,6 +27,26 @@ pub struct ProcessHtmlOption {
     is_sxg: bool,
 }
 
+#[derive(Debug, PartialEq, Eq)]
+enum ContentType {
+    HtmlUtf8,
+    HtmlOther,
+    Other,
+}
+fn parse_content_type(content_type_header_value: &str) -> ContentType {
+    static CONTENT_TYPE_HTML: Lazy<Regex> =
+        Lazy::new(|| Regex::new(r#"^text/html([ \t]*;[ \t]*charset=(utf-8|"utf-8"))?$"#).unwrap());
+    if let Some(captures) = CONTENT_TYPE_HTML.captures(content_type_header_value) {
+        if captures.get(1).is_some() {
+            ContentType::HtmlUtf8
+        } else {
+            ContentType::HtmlOther
+        }
+    } else {
+        ContentType::Other
+    }
+}
+
 /// Processes HTML using the following processors.
 /// - For `<link rel=preload>` elements, they are promoted to Link headers.
 /// - For `<template data-sxg-only>` elements:
@@ -32,19 +54,61 @@ pub struct ProcessHtmlOption {
 ///   - Else, they are deleted.
 /// - For `<script data-issxg-var>` elements, they are replaced with
 ///   `<script>window.isSXG=...</script>`, where `...` is true or false.
-/// This function tries the input body as UTF8 string, if input body is not valid UTF8, the input
-/// will be returned back without any HTML processing.
+/// If input charset is not UTF8, the input will be returned back without any HTML.
 pub fn process_html(input: HttpResponse, option: ProcessHtmlOption) -> Result<HttpResponse> {
+    let content_type_header = input.headers.iter().find_map(|(name, value)| {
+        if name.eq_ignore_ascii_case("content-type") {
+            Some(value)
+        } else {
+            None
+        }
+    });
+    let mut known_utf8 = false;
+    if let Some(content_type_header) = content_type_header {
+        match parse_content_type(content_type_header) {
+            ContentType::HtmlUtf8 => known_utf8 = true,
+            ContentType::HtmlOther => (),
+            ContentType::Other => return Ok(input),
+        };
+    } else {
+        // Doesn't process HTML because content-type header does not exsist.
+        return Ok(input);
+    }
     let input_body = match String::from_utf8(input.body) {
         Ok(input_body) => input_body,
-        Err(e) => return Ok(HttpResponse {
-            body: e.into_bytes(),
-            headers: input.headers,
-            status: input.status,
-        }),
+        Err(e) => {
+            return Ok(HttpResponse {
+                body: e.into_bytes(),
+                headers: input.headers,
+                status: input.status,
+            })
+        }
     };
     let mut link_headers: Vec<String> = vec![];
     let element_content_handlers = vec![
+        // Parse the meta tag, per the implementation in HTMLMetaCharsetParser::CheckForMetaCharset:
+        // https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/html/parser/html_meta_charset_parser.cc;l=62-125;drc=7a0b88f6d5c015fd3c280b58c7a99d8e1dca28ac.
+        // This differs slightly from what's described at https://github.com/whatwg/html/issues/6962, and
+        // differs drastically from what's specified in
+        // https://html.spec.whatwg.org/multipage/parsing.html#prescan-a-byte-stream-to-determine-its-encoding.
+        lol_html::element!("meta", |e| {
+            // EncodingFromMetaAttributes:
+            // https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/html/parser/html_parser_idioms.cc;l=362-393;drc=7a0b88f6d5c015fd3c280b58c7a99d8e1dca28ac
+            if let Some(charset) = e.get_attribute("charset") {
+                if charset.eq_ignore_ascii_case("utf-8") {
+                    known_utf8 = true;
+                }
+            } else if let (Some(http_equiv), Some(content)) =
+                (e.get_attribute("http-equiv"), e.get_attribute("content"))
+            {
+                if http_equiv.eq_ignore_ascii_case("content-type")
+                    && parse_content_type(&content) == ContentType::HtmlUtf8
+                {
+                    known_utf8 = true;
+                }
+            }
+            Ok(())
+        }),
         lol_html::element!(
             r#"link[rel~="preload" i][href][as]:not([data-sxg-no-header])"#,
             |e| {
@@ -91,6 +155,13 @@ pub fn process_html(input: HttpResponse, option: ProcessHtmlOption) -> Result<Ht
             ..lol_html::Settings::default()
         },
     )?;
+    if !known_utf8 {
+        return Ok(HttpResponse {
+            headers: input.headers,
+            status: input.status,
+            body: input_body.into_bytes(),
+        });
+    }
     let mut output_headers = input.headers;
     output_headers.push(("Link".to_string(), link_headers.join(",")));
     Ok(HttpResponse {
@@ -98,4 +169,71 @@ pub fn process_html(input: HttpResponse, option: ProcessHtmlOption) -> Result<Ht
         headers: output_headers,
         status: input.status,
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_parses_content_type() {
+        assert_eq!(parse_content_type(r#"text/html"#), ContentType::HtmlOther);
+        assert_eq!(
+            parse_content_type(r#"text/html; charset=utf-8"#),
+            ContentType::HtmlUtf8
+        );
+        assert_eq!(
+            parse_content_type(r#"text/html; charset="utf-8""#),
+            ContentType::HtmlUtf8
+        );
+        assert_eq!(parse_content_type(r#"text/plain"#), ContentType::Other);
+    }
+    fn quick_process(content_type: &str, input_body: &str) -> String {
+        let output = process_html(
+            HttpResponse {
+                status: 200,
+                headers: vec![("content-type".to_string(), content_type.to_string())],
+                body: input_body.to_string().into_bytes(),
+            },
+            ProcessHtmlOption { is_sxg: true },
+        );
+        String::from_utf8(output.unwrap().body).unwrap()
+    }
+    #[test]
+    fn it_works() {
+        assert_eq!(
+            quick_process(
+                "text/html;charset=utf-8",
+                "<script data-issxg-var></script>",
+            ),
+            "<script data-issxg-var>window.isSXG=true</script>"
+        );
+        // HTML is not processed when charset is not specified.
+        assert_eq!(
+            quick_process("text/html", "<script data-issxg-var></script>",),
+            "<script data-issxg-var></script>",
+        );
+        // Meta tag of charset is supported.
+        assert_eq!(
+            quick_process(
+                "text/html",
+                "<meta http-equiv=content-type content=\"text/html;charset=utf-8\">\
+                <script data-issxg-var></script>"
+            ),
+            "<meta http-equiv=content-type content=\"text/html;charset=utf-8\">\
+            <script data-issxg-var>window.isSXG=true</script>"
+        );
+        // https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/html/parser/html_parser_idioms.cc;l=308-354;drc=984c3018ecb2ff818e900fdb7c743fc00caf7efe
+        // https://html.spec.whatwg.org/multipage/urls-and-fetching.html#extracting-character-encodings-from-meta-elements
+        // lol-html doesn't appear to decode HTML entities inside
+        // attribute values, so this won't work. This could be supported in the future.
+        assert_eq!(
+            quick_process(
+                "text/html",
+                "<meta http-equiv=content-type content=\"text/html;charset=&quot;utf-8&quot;\">\
+                <script data-issxg-var></script>"
+            ),
+            "<meta http-equiv=content-type content=\"text/html;charset=&quot;utf-8&quot;\">\
+            <script data-issxg-var></script>",
+        );
+    }
 }
