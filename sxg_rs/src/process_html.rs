425987cc1c87fb05157981b6ec45e6a9be87e77f
Quang Luong <quang@cloudflare.com>
2022-04-04 14:53:00 +0100

    Fix typos in process_html documentation

--- sxg_rs/src/process_html.rs
+++ sxg_rs/src/process_html.rs
@@ -55,7 +55,7 @@ fn parse_content_type(content_type_header_value: &str) -> ContentType {
 /// Processes HTML using the following processors.
 /// - For `<link rel=preload>` elements, they are promoted to Link headers.
 /// - For `<template data-sxg-only>` elements:
-///   - If SXG, they are "unwrapped" (i.e. their children promoted out of the <teplate>).
+///   - If SXG, they are "unwrapped" (i.e. their children are promoted out of the <template>).
 ///   - Else, they are deleted.
 /// - For `<script data-issxg-var>` elements, they are replaced with
 ///   `<script>window.isSXG=...</script>`, where `...` is true or false.
