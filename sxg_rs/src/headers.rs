9b4ecb2645e6d147e18cc79ff27493af4c5edda8
Devin Mullins <twifkak@google.com>
2022-05-24 10:50:22 -0700

    Allow SXGs to Vary by Cookie. (#275)
    SXGs with `Vary: Cookie` will only be shown to users without cookies for the
    signed domain, per https://crbug.com/1250532. This will soon be allowed by
    https://github.com/google/webpackager/blob/main/docs/cache_requirements.md.

--- sxg_rs/src/headers.rs
+++ sxg_rs/src/headers.rs
@@ -133,11 +133,9 @@ impl Headers {
             // TODO: Remove this section once https://crbug.com/1250532 is fixed in most clients.
             if let Some(vary) = self.0.get("vary") {
                 if let Ok(directives) = parse_vary_header(vary) {
-                    if directives.contains(&"*")
-                        || directives.iter().any(|d| d.eq_ignore_ascii_case("cookie"))
-                    {
+                    if directives.contains(&"*") {
                         return Err(anyhow!(
-                            "The response may vary by cookie, \
+                            "The response may vary by anything,\
                             because its \"vary\" header is \"{}\".",
                             vary
                         ));
