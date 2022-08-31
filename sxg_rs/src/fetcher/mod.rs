3fc26c4d5a4a6d20644a797e9b35373636e4c8e2
Boxiao Cao <9083193+antiphoton@users.noreply.github.com>
2022-08-31 14:20:46 -0700

    Allow using get function on dynamic fetcher

--- sxg_rs/src/fetcher/mod.rs
+++ sxg_rs/src/fetcher/mod.rs
@@ -26,23 +26,25 @@ use async_trait::async_trait;
 #[cfg_attr(not(feature = "wasm"), async_trait)]
 pub trait Fetcher: MaybeSend + MaybeSync {
     async fn fetch(&self, request: HttpRequest) -> Result<HttpResponse>;
-    /// Uses `Get` method and returns response body.
-    // `where Self: Sized` is because of
-    // https://github.com/rust-lang/rust/issues/51443 and in particular
-    // https://docs.rs/async-trait/0.1.36/async_trait/#dyn-traits.
-    async fn get(&self, url: &str) -> Result<Vec<u8>>
-    where
-        Self: Sized,
-    {
-        let request = HttpRequest {
-            body: vec![],
-            headers: vec![],
-            method: crate::http::Method::Get,
-            url: url.to_string(),
-        };
-        let response = self.fetch(request).await?;
-        Ok(response.body)
-    }
+}
+
+/// Uses `Get` method and returns response body.
+/// - Why this function is not put inside [`Fetcher`] trait?
+///   If we declare `Fetcher::get` function with a default implementation,
+///   we have to also add a constraint `where Self: Sized` to `Fetcher::get`,
+///   because of https://github.com/rust-lang/rust/issues/51443 and in particular
+///   https://docs.rs/async-trait/0.1.57/async_trait/#dyn-traits.
+///   However, having such constraint `Self: Sized` prevent using `Fetcher::get` method on a
+///   `dyn Fetcher` variable, because `dyn Fetcher` is not `Sized`.
+pub async fn get(fetcher: &dyn Fetcher, url: impl ToString) -> Result<Vec<u8>> {
+    let request = HttpRequest {
+        body: vec![],
+        headers: vec![],
+        method: crate::http::Method::Get,
+        url: url.to_string(),
+    };
+    let response = fetcher.fetch(request).await?;
+    Ok(response.body)
 }
 
 pub const NULL_FETCHER: NullFetcher = NullFetcher {};
